d9de02f0f917d0a9255fc0e71d01c4038faf1536
sdfw <sdf@sd.com>
2020-04-10 16:11:25 +0800

    first commit

---
+++ pages/post.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/Mylayout';
+
+const Page = () => {
+    const router = useRouter();
+    return (
+        <Layout>
+            <h1>{router.query.title}</h1>
+            <p>This is the blog post content.</p>
+        </Layout>
+    );
+};
+
+export default Page;
